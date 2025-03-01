a77b3e6c91e42a5eb0c56fa3e465cd2fe8184464
Seongjun-K <seongjunkang1@gmail.com>
2025-02-28 19:20:46 -0800

    changed trial number and batch size for trial #8

--- information.py
+++ information.py
@@ -1,5 +1,5 @@
-TRIAL_NUMBER = 7
+TRIAL_NUMBER = 8
 MODEL_SAVE_PATH = f'saved_models/cnn_trial_{TRIAL_NUMBER}.keras'
 HISTORY_SAVE_PATH = f'saved_histories/training_history_trial_{TRIAL_NUMBER}.pkl'
-BATCH = 8
+BATCH = 16
 EPOCH = 50
