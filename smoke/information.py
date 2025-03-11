a444d25c769e65ecc892d1fe41e152b370e0406e
Seongjun-K <seongjunkang1@gmail.com>
2025-03-10 21:20:31 -0700

    latest information about our model

--- smoke/information.py
+++ smoke/information.py
@@ -1,4 +1,4 @@
-TRIAL = 13   # MODIFY THIS BEFORE EVERY RUN
+TRIAL = 8   # MODIFY THIS BEFORE EVERY RUN
 MODEL_SAVE_PATH = f'saved_models/dehazer_trial_{TRIAL}.keras'
 HISTORY_SAVE_PATH = f'saved_histories/dehazer_history_trial{TRIAL}.pkl'
 NUM_EPOCHS = 50
