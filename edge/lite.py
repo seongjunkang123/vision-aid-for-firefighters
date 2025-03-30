448611e7884b97cc620b25112d677ceffa97ceec
Seongjun-K <seongjunkang1@gmail.com>
2025-03-29 22:01:12 -0700

    lite try 2

--- edge/lite.py
+++ edge/lite.py
@@ -4,8 +4,9 @@ import information
 path = information.MODEL_SAVE_PATH
 model = tf.keras.models.load_model(path)
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
+converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS]
 
 tflite_model = converter.convert()
-with open('model.tflite', 'wb') as f:
+with open('model_1.tflite', 'wb') as f:
     f.write(tflite_model)
 
