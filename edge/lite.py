8c7ec235fdf72694ffcaeaf5cead25d0230112b4
seongjunkang123 <seongjunk123@gmail.com>
2025-04-09 20:54:40 -0700

    Added full quantization - Weights are uint8 - input and output datas are uint8

--- edge/lite.py
+++ edge/lite.py
@@ -1,15 +1,23 @@
 import tensorflow as tf
 import information
 import lite_info
+import numpy as np
 
 path = information.MODEL_SAVE_PATH
 model = tf.keras.models.load_model(path)
 
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
-converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS]
 converter.optimizations = [tf.lite.Optimize.DEFAULT]
-converter.experimental_new_converter = True
-converter._experimental_lower_tensor_list_ops = True
+
+def representative_dataset_gen():
+    for _ in range(100):
+        data = np.random.rand(1, 256, 256, 3).astype('float32')
+        yield [data]
+
+converter.representative_dataset = representative_dataset_gen
+
+converter.inference_input_type = tf.uint8
+converter.inference_output_type = tf.uint8
 
 tflite_model = converter.convert()
 
@@ -17,4 +25,3 @@ with open(lite_info.MODEL_SAVE_PATH, 'wb') as f:
     f.write(tflite_model)
 
 print(f"Model Version {lite_info.VERSION}")
-lite_info.VERSION += 1
